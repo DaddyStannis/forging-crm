4952ba65bb033b4884d7e09a7ad87a3191f4db6c
DaddyStannis <stanislav.sakun@ukr.net>
2024-08-23 14:40:35 +0300

    Removed old files

--- entities/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./Batch";
-export * from "./Client";
-export * from "./OrderLine";
-export * from "./Product";
--- micro-orm.config.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv-flow";
-dotenv.config();
-import config from "./config/micro-orm";
-
-export default config;
